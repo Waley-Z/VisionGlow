378dcad9ed80c927362e569672e9fbb112bbafcb
waleyz <hongxiao.zheng.gr@dartmouth.edu>
2025-02-05 21:36:55 -0500

    Initial Commit

---
+++ VisionGlow/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  VisionGlow
+//
+//  Created by DEV Studio on 2/5/25.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ VisionGlow/VisionGlowApp.swift
@@ -0,0 +1,33 @@
+//
+//  VisionGlowApp.swift
+//  VisionGlow
+//
+//  Created by DEV Studio on 2/5/25.
+//
+
+import SwiftUI
+
+@main
+struct VisionGlowApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
